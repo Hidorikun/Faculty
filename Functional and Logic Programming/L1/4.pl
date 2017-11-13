550a783036f04e866de11a2fd1e3a39b74604bcd
George Vele <george.vele@yardi.com>
2017-11-13 17:16:22 +0200

    Added some problems.

---
+++ Functional and Logic Programming/L1/4.pl
@@ -0,0 +1,41 @@
+%4.
+%a. Write a predicate to determine the difference of two sets.
+%	A - B means elements of A which are not the elements of B.
+%b. Write a predicate to add value 1 after every even element from a list.
+
+%a.
+
+%contains(+L:list, +V:atom)
+%contains(l1..ln, v) = false, n == 0
+%					   true, l1 == v
+%					   contains(l2..ln, v), otherwise
+
+contains([V|_], V):-!.
+contains([_|T], V):-contains(T, V).
+
+%difference(+SET1:list, +SET2:list, -DIF:list)
+%difference(l1..ln, k1..km) = [], n == 0
+%							  difference(l2..ln, k1..kn)), if contains(k1..kn, l1)
+%							  l1 + difference(l2..ln, k1..kn),otherwise
+
+difference([], _, []).
+difference([H|T], SET, R):-
+    contains(SET, H),
+    difference(T, SET, R), !.
+difference([H|T], SET,[H|R]):-
+    difference(T, SET, R).
+
+%b.
+
+%add(+L:list, -R:list)
+%add(l1..ln) = [], n == 0
+%		     = l1 + add(l2..ln), if l1 mod 2 != 0
+%		     = l1 + 1 + add(l2..ln), if l1 mod 2 == 0
+
+add([], []).
+add([H|T], [H|R]):-
+    H mod 2 =\= 0,
+    add(T, R).
+add([H|T], [H,1|R]):-
+    H mod 2 =:= 0,
+    add(T, R).
---
+++ Functional and Logic Programming/L1/5.pl
@@ -0,0 +1,54 @@
+%5.
+%a. Write a predicate to compute the union of two sets.
+%b. Write a predicate to determine the set of all the pairs of elements in a list.
+%Eg.: L = [a b c d] => [[a b] [a c] [a d] [b c] [b d] [c d]].
+
+%a.
+
+%remove_occurences(+L:list, +V:atom, -R:list)
+%remove_occurences(l1..ln, v) = [], n = 0
+%							    l1 + remove_occurences(l2..ln, v), l1 != v
+%							   	remove_occurences(l2..ln, v), l1 == v
+
+remove_occurences([], _, []).
+remove_occurences([H|T], V, [H|R]):-H=\=V,
+    remove_occurences(T, V, R).
+remove_occurences([H|T], V, R):-H=:=V,
+    remove_occurences(T, V, R).
+
+%union(+SET1:list, +SET2:list, -R:list)
+%union(l1..ln, k1..km ) = [], if n = 0 && m = 0
+%					    = l1 + union(remove_occurences(l2..ln, l1), remove_occurences(k1..km, l1)),
+%					    		if n > 0
+%					    = k1 + union(remove_occurences(l1..ln, l1), remove_occurences(k1..km, l1)),
+%					    		if m > 0
+
+union([], [], []).
+union([H|T], SET2, [H|R]):-
+    remove_occurences(T, H, R1),
+    remove_occurences(SET2, H, R2),
+    union(R1, R2, R).
+union([], SET2, R):-
+    union(SET2, [], R).
+
+%b.
+
+%sets(+L:list, -R:list)
+%sets(l1..ln, k) = [], k == 0
+%			     = l1 + sets(l2..ln, k-1), if k > 0
+%			     = sets(l2..ln, k-1), if k > 0
+
+gen_sets(_, 0, []):-!.
+gen_sets([H|T], K, [H|R]):-
+    K1 is K - 1,
+    gen_sets(T, K1, R).
+gen_sets([_|T], K, R):-
+    gen_sets(T, K, R).
+
+%gen_sets_list(+L:list, -R:list)
+%gen_sets_list(l1..ln) = [], n == 0
+%					   = findall(sets(l1..ln, 2))
+
+sets([], []).
+sets(LIST, R):-
+	findall(X, gen_sets(LIST, 2, X), R).
