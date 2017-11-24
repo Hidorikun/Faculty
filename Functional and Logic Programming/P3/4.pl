5509dc2d5c0f8625a8275be2301d44105c2111e9
Hidorikun <vele_george@yahoo.ro>
2017-11-24 10:08:11 +0200

    Updated 4.pl

--- Functional and Logic Programming/P3/4.pl
+++ Functional and Logic Programming/P3/4.pl
@@ -1,8 +1,8 @@
 %4.
-%The list a1... an is given.
+%The list a1...an is given.
 %Write a predicate to determine all sublists strictly ascending of this list a
 
-%subset(+LIST, -LIST)
+%subset(+L:list, -R:list)
 %subset(l1..ln) = [], n = 0
 %				  l1 U subset(l2..ln)
 %				  subset(l2..ln)
@@ -13,16 +13,17 @@ subset([E|Tail], [E|NTail]):-
 subset([_|Tail], NTail):-
   subset(Tail, NTail).
 
-%is_sorted(+LIST, -BOOL)
-%is_sorted(l1..ln) = True, n = 0
+%is_sorted(+L:list)
+%is_sorted(l1..ln) = True, n = 0 || n = 1
 %					 True, n = 1
 %					 False, l1 > l2
 %					 is_sorted(l2..ln), l2 >= l1
 
-is_sorted([], true).
-is_sorted([_], true).
-is_sorted([H1, H2|_], false):- H1 > H2.
-is_sorted([H1, H2|T], R):- H1 =< H2, is_sorted([H2|T], R).
+is_sorted([]):-!.
+is_sorted([_]):-!.
+is_sorted([H1, H2|T]):-
+	H1 =< H2,
+	is_sorted([H2|T]).
 
 %process(+LIST, -SortedSubset)
 %process(l1..ln) = [], n = 0
@@ -30,5 +31,5 @@ is_sorted([H1, H2|T], R):- H1 =< H2, is_sorted([H2|T], R).
 
 process([], []).
 process(L, K):-
-    subset(L, K),
-    is_sorted(K, true).
+	subset(L, K),
+	is_sorted(K).
