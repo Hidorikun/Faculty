80298396c43d6e358c814477c897a671581768c9
Hidorikun <vele_george@yahoo.ro>
2017-11-24 11:59:08 +0200

    Update 4.p.

--- Functional and Logic Programming/P3/4.pl
+++ Functional and Logic Programming/P3/4.pl
@@ -4,8 +4,8 @@
 
 %subset(+L:list, -R:list)
 %subset(l1..ln) = [], n = 0
-%				  l1 U subset(l2..ln)
-%				  subset(l2..ln)
+%				 = l1 U subset(l2..ln)
+%				 = subset(l2..ln)
 
 subset([], []).
 subset([E|Tail], [E|NTail]):-
@@ -15,9 +15,9 @@ subset([_|Tail], NTail):-
 
 %is_sorted(+L:list)
 %is_sorted(l1..ln) = True, n = 0 || n = 1
-%					 True, n = 1
-%					 False, l1 > l2
-%					 is_sorted(l2..ln), l2 >= l1
+%				   = True, n = 1
+%				   = False, l1 > l2
+%				   = is_sorted(l2..ln), l2 >= l1
 
 is_sorted([]):-!.
 is_sorted([_]):-!.
@@ -25,9 +25,9 @@ is_sorted([H1, H2|T]):-
 	H1 =< H2,
 	is_sorted([H2|T]).
 
-%process(+LIST, -SortedSubset)
+%process(+L:list, -R:list)
 %process(l1..ln) = [], n = 0
-%				   subset(l1..ln), is_sorted(subset(l1..ln))
+%				 = subset(l1..ln), is_sorted(subset(l1..ln))
 
 process([], []).
 process(L, K):-
