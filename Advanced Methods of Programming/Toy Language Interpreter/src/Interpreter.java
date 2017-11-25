3b1ba5ba6354a35675a54f43c481c9594888c605
Hidorikun <vele_george@yahoo.ro>
2017-11-25 14:20:44 +0200

    Cleaned a bit.

--- Advanced Methods of Programming/Toy Language Interpreter/src/Interpreter.java
+++ Advanced Methods of Programming/Toy Language Interpreter/src/Interpreter.java
@@ -216,7 +216,6 @@ public class Interpreter {
                                                                 new ReadHeap("a")
                                                         )
                                                 )
-
                                         )
                                 )
                         )
