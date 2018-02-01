62b9a27312a4ba445c4c7bec5c6fe4d7ad4398e5
Hidorikun <vele_george@yahoo.ro>
2018-02-01 20:02:22 +0200

    Add 15.pl

---
+++ Functional and Logic Programming/P3/15.pl
@@ -0,0 +1,51 @@
+% 15. For a given n, positive, determine all decomposition of n as a sum of consecutive natural numbers.
+
+%sublists(+L:list, -R:list)
+
+sublists([], []).
+sublists([H|T], [H|R]):-
+	sublists(T, R).
+sublists([_|T], R):-
+	sublists(T, R).
+
+%consecutive(+L:list)
+
+consecutive([_]):-!.
+consecutive([H1, H2|T]):-
+	DIFF is H2 - H1,
+	DIFF =:= 1,
+	consecutive([H2|T]).
+
+%sum(+L:list, -R:number)
+
+sum([], 0).
+sum([H|T], SUM):-
+	sum(T, AUX),
+	SUM is H + AUX.
+
+%range_(+N:number, -R:list)
+
+range_(0, []):-!.
+range_(N, R):-
+	M is N -1,
+	range_(M, AUX),
+	append(AUX, [N], R).
+
+%range(+N:number, -R:list)
+
+range(N, R):-
+	M is N - 1,
+	range_(M, R).
+
+%process(+N:number, -R:list)
+
+process(N, R):-
+	range(N, RANGE),
+	sublists(RANGE, R),
+	consecutive(R),
+	sum(R, N).
+
+%allsolutions(+N:number, -R:list)
+
+all_solutions(N, R):-
+	findall(AUX, process(N, AUX), R).
