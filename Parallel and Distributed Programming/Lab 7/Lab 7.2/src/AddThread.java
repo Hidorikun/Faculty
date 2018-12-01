6fb19dc4bec5f7c94b6e22aa5b2a78a727783a5a
Vele George <georgevele2016@gmail.com>
2018-12-01 15:43:20 +0200

    Add PPD lab 7.2 testing

--- Parallel and Distributed Programming/Lab 7/Lab 7.2/src/AddThread.java
+++ Parallel and Distributed Programming/Lab 7/Lab 7.2/src/AddThread.java
@@ -3,159 +3,92 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class AddThread implements Runnable {
-    private ArrayBlockingQueue<BigInteger> queueIN = null;
-    private ArrayBlockingQueue<BigInteger> queueOUT = null;
+    private ArrayBlockingQueue<BigInteger> queueIN;
+    private ArrayBlockingQueue<BigInteger> queueOUT;
     private BigInteger number;
-    private BigInteger secondNumber = null;
     private BigInteger digitPos = BigInteger.ONE;
     private BigInteger remainder = BigInteger.ZERO;
-    private String name;
+    private BigInteger sum = BigInteger.ZERO;
 
-    public AddThread(String name, BigInteger number, BigInteger secondNumber, ArrayBlockingQueue<BigInteger> queueOUT) {
+
+    public AddThread(BigInteger number, BigInteger secondNumber, ArrayBlockingQueue<BigInteger> queueOUT) {
+        this.queueIN = new ArrayBlockingQueue<BigInteger>(secondNumber.toString().length() + 1);
         this.queueOUT = queueOUT;
         this.number = number;
-        this.secondNumber = secondNumber;
-        this.name = name;
+        this.splitNumberInQueueIN(secondNumber);
     }
 
-    public AddThread(String name, BigInteger number ,ArrayBlockingQueue<BigInteger> queueIN, ArrayBlockingQueue<BigInteger> queueOUT) {
+    public AddThread(BigInteger number ,ArrayBlockingQueue<BigInteger> queueIN, ArrayBlockingQueue<BigInteger> queueOUT) {
         this.queueIN = queueIN;
         this.queueOUT = queueOUT;
         this.number = number;
-        this.name = name;
     }
 
-    void normalCode() throws InterruptedException {
+
+    @Override
+    public void run() {
+        try {
+            addition();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void addition() throws InterruptedException {
 
         BigInteger digit = this.queueIN.poll(5, TimeUnit.SECONDS);
-
-//        System.out.println(name + " got " + digit);
-
 
         while (digit.compareTo(new BigInteger("-1")) != 0) {
             this.digitPos = this.digitPos.multiply(BigInteger.TEN);
 
-            BigInteger sum = digit
+            sum = digit
                     .add(this.number.mod(this.digitPos).divide(this.digitPos.divide(BigInteger.TEN)))
                     .add(this.remainder);
 
-            if (sum.compareTo(BigInteger.TEN) >= 0) {
-                remainder = sum.divide(BigInteger.TEN);
-                sum = sum.mod(BigInteger.TEN);
-            } else {
-                remainder = BigInteger.ZERO;
-            }
-//            System.out.println(name + " --> " + sum);
+            this.setRemainderAndSum();
             queueOUT.offer(sum);
 
             digit = this.queueIN.poll(5, TimeUnit.SECONDS);
-//            System.out.println(name + " got " + digit);
         }
 
         while (this.digitPos.compareTo(this.number) <= 0 || this.remainder.compareTo(BigInteger.ZERO) != 0){
             if (this.digitPos.compareTo(this.number) <= 0) {
                 this.digitPos = this.digitPos.multiply(BigInteger.TEN);
 
-                BigInteger sum = digit
+                sum = digit
                         .add(this.number.mod(this.digitPos).divide(this.digitPos.divide(BigInteger.TEN)))
                         .add(this.remainder);
 
-                if (sum.compareTo(BigInteger.TEN) >= 0) {
-                    remainder = sum.divide(BigInteger.TEN);
-                    sum = sum.mod(BigInteger.TEN);
-                } else {
-                    remainder = BigInteger.ZERO;
-                }
-//                System.out.println(name + " --> " + sum);
+                this.setRemainderAndSum();
                 queueOUT.offer(sum);
 
             } else {
-//                System.out.println(name + " --> " + remainder);
                 queueOUT.offer(this.remainder);
                 this.remainder = BigInteger.ZERO;
             }
 
         }
 
-//        System.out.println(name + " --> " + "-1");
-        queueOUT.offer(new BigInteger("-1"));
-
-
-    }
-
-    void leafCode() {
-        while (this.number.compareTo(BigInteger.ZERO) != 0 && this.secondNumber.compareTo(BigInteger.ZERO) != 0) {
-
-            BigInteger sum = this.number.mod(BigInteger.TEN)
-                    .add(this.secondNumber.mod(BigInteger.TEN))
-                    .add(this.remainder);
-//
-//            getDigitAndRemainderFromSum(sum);
-            if (sum.compareTo(BigInteger.TEN) >= 0) {
-                remainder = sum.divide(BigInteger.TEN);
-                sum = sum.mod(BigInteger.TEN);
-            } else {
-                remainder = BigInteger.ZERO;
-            }
-
-            queueOUT.offer(sum);
-
-            this.number = this.number.divide(BigInteger.TEN);
-            this.secondNumber = this.secondNumber.divide(BigInteger.TEN);
-        }
-
-        while (this.number.compareTo(BigInteger.ZERO) != 0){
-            BigInteger sum = this.number.mod(BigInteger.TEN)
-                    .add(this.remainder);
-
-            if (sum.compareTo(BigInteger.TEN) >= 0) {
-                remainder = sum.divide(BigInteger.TEN);
-                sum = sum.mod(BigInteger.TEN);
-            } else {
-                remainder = BigInteger.ZERO;
-            }
-
-//            System.out.println(name + " --> " + sum);
-            queueOUT.offer(sum);
-        }
-
-        while (this.number.compareTo(BigInteger.ZERO) != 0){
-            BigInteger sum = this.secondNumber.mod(BigInteger.TEN)
-                    .add(this.remainder);
-
-            if (sum.compareTo(BigInteger.TEN) >= 0) {
-                remainder = sum.divide(BigInteger.TEN);
-                sum = sum.mod(BigInteger.TEN);
-            } else {
-                remainder = BigInteger.ZERO;
-            }
-
-//            System.out.println(name + " --> " + sum);
-            queueOUT.offer(sum);
-        }
-
-        if (this.remainder.compareTo(BigInteger.ZERO) != 0) {
-//            System.out.println(name + " --> " + remainder);
-            queueOUT.offer(this.remainder);
-        }
-//        System.out.println(name + " --> " + "-1");
         queueOUT.offer(new BigInteger("-1"));
     }
 
-    @Override
-    public void run() {
-        if (queueIN != null) {
-            try {
-                normalCode();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }else {
-            leafCode();
+    void setRemainderAndSum(){
+        if (sum.compareTo(BigInteger.TEN) >= 0) {
+            remainder = sum.divide(BigInteger.TEN);
+            sum = sum.mod(BigInteger.TEN);
+        } else {
+            remainder = BigInteger.ZERO;
         }
     }
 
-    void getDigitAndRemainderFromSum(BigInteger sum) {
+    void splitNumberInQueueIN(BigInteger number) {
+        this.queueIN.clear();
 
+        while (number.compareTo(BigInteger.ZERO) != 0) {
+            this.queueIN.add(number.mod(BigInteger.TEN));
+            number = number.divide(BigInteger.TEN);
+        }
+
+        this.queueIN.add(new BigInteger("-1"));
     }
 }
--- Parallel and Distributed Programming/Lab 7/Lab 7.2/src/Main.java
+++ Parallel and Distributed Programming/Lab 7/Lab 7.2/src/Main.java
@@ -8,37 +8,42 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-        int N = 4;
+        int TEST_COUNT = 16;
+        int QUEUE_CAP = 20;
 
-        String result = "";
+        batchTesting(TEST_COUNT, QUEUE_CAP);
 
-        List<BigInteger> numbers = new ArrayList<>(N);
+    }
 
-        for (int i = 0; i < N; i++){
-            numbers.add(new BigInteger(randomNumber(5)));
+    static void addNumbersSequential(List<BigInteger> numbers) {
+        BigInteger sum = BigInteger.ZERO;
+        for (BigInteger number : numbers) {
+            sum = sum.add(number);
         }
+    }
 
-        System.out.println("numbers = " + numbers);
+    static void addNumbersParallel(List<BigInteger> numbers, int queueCap){
+        int N = numbers.size();
 
         List<ArrayBlockingQueue<BigInteger>> queues = new ArrayList<>(N - 1);
 
         for (int i = 0; i < N - 1; i++){
-            queues.add(new ArrayBlockingQueue(20));
+            queues.add(new ArrayBlockingQueue<BigInteger>(queueCap));
         }
 
         List<Thread> threads = new ArrayList<>(N - 1);
 
-        threads.add(new Thread(new AddThread("thread 0", numbers.get(0), numbers.get(1), queues.get(0))));
+        threads.add(new Thread(new AddThread(numbers.get(0), numbers.get(1), queues.get(0))));
 
         for (int i = 2; i < N; i++){
-            threads.add(new Thread(new AddThread("thread " + (i - 1),  numbers.get(i), queues.get(i - 2), queues.get(i - 1))));
+            threads.add(new Thread(new AddThread(numbers.get(i), queues.get(i - 2), queues.get(i - 1))));
         }
 
         for (int i = 0; i < N - 1; i++){
-//            System.out.println("starting thread " + i);
             threads.get(i).start();
         }
 
+        String result = "";
         BigInteger digit = new BigInteger("0");
         while (digit.compareTo(new BigInteger("-1")) != 0) {
             try {
@@ -52,22 +57,51 @@ public class Main {
             }
         }
 
-
-        for (int i = 0; i < N - 2; i++){
+        for (int i = 0; i < N - 1; i++){
             try {
                 threads.get(i).join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
 
-        BigInteger sum = BigInteger.ZERO;
-        for (BigInteger number : numbers) {
-            sum = sum.add(number);
+    static void testParallel(int level, List<BigInteger> numbers, int queueCap) {
+        long startTime = System.nanoTime();
+        addNumbersParallel(numbers, queueCap);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        System.out.println("Level " + level + ": " + duration + " ms");
+
+    }
+
+    static void testSequential(int level, List<BigInteger> numbers) {
+        long startTime = System.nanoTime();
+        addNumbersSequential(numbers);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+        System.out.println("Level " + level + ": " + duration + " ms");
+
+    }
+
+    static void batchTesting(int testCount, int queueCap) {
+        List<BigInteger> numbers;
+
+        for (int i = 1; i <= testCount; i++){
+            numbers = generateNumbers(i * 10, i * 1);
+            testParallel(i, numbers, queueCap);
+            testSequential(i, numbers);
+        }
+    }
+
+    static List<BigInteger> generateNumbers(int count, int digitSize) {
+        List<BigInteger> numbers = new ArrayList<>(count);
+
+        for (int i = 0; i < count; i++){
+            numbers.add(new BigInteger(randomNumber(digitSize)));
         }
 
-        System.out.println("My result : " + result);
-        System.out.println("Actual sum: " + sum);
+        return numbers;
     }
 
     static String randomNumber(int length) {
